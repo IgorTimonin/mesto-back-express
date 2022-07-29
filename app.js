0c56e8fbf8cf266a94186dc8f30cc7c2378163a3
IgorTimonin <mut4nt@yandex.ru>
2022-07-29 16:28:36 +0300

    mongoose install

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const { PORT = 3000 } = process.env;
 const app = express();
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:3000/mestodb", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+app.get('/', (req, res) => {
+  res.send('Привет, юзер');
+})
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
