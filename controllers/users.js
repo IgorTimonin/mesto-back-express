76269fbb02f4ae3864b8dd93373da426fc317cd7
IgorTimonin <mut4nt@yandex.ru>
2022-08-18 17:27:54 +0300

    eslint --fix

--- controllers/users.js
+++ controllers/users.js
@@ -168,7 +168,7 @@ module.exports.login = (req, res, next) => {
       const token = jwt.sign(
         { _id: user._id },
         NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
-        { expiresIn: '7d' }
+        { expiresIn: '7d' },
       );
       if (!token) {
         throw new UnauthorizedError('Ошибка при создании токена');
